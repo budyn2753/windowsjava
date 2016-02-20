3eafdd1cf1fe8a92ff7e3f9766c6ccbc8f13361c
budyn2753 <grzelak.dariusz93@gmail.com>
2016-02-20 21:39:58 +0100

    kurwaprawiedzial

--- Scheduler/src/proces.java
+++ Scheduler/src/proces.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Vector;
@@ -57,12 +58,12 @@ public static Boolean NiePustaKolejka[] = new Boolean[16];
 public ArrayList<proces> KolejkaGotowychProcesow = new ArrayList<proces>();
 //public ArrayList<proces>KolejkaProcesow= new ArrayList<proces>();
 public static ArrayList<List<proces>> tab ;
-static ArrayList<proces> group[] = new ArrayList[16];
-public static ListIterator it ;
+static LinkedList<proces> group[] = new LinkedList[16];
+public static ListIterator it  ;
 public static int ZnajdzMaxPriorytet(){
 	
 	for (int i =15; i>0;i--){
-		if(NiePustaKolejka[i]=true){
+		if(NiePustaKolejka[i]==true){
 			return i;
 		}
 	}
@@ -85,7 +86,7 @@ public static void SetZeroBitTabPriority(int p){
 		NiePustaKolejka[p]= true;
 	}
 }
-public static void NowyProces(int id,int priorytet,int czasPrzyjscia, int rozmiar){
+public static void NowyProces(int priorytet,int rozmiar, int id,int czasPrzyjscia, int czas){
 	//tab.
 	proces e = new proces(priorytet, czasPrzyjscia,rozmiar,id);
 	group[priorytet].add(e);
@@ -94,7 +95,7 @@ public static void NowyProces(int id,int priorytet,int czasPrzyjscia, int rozmia
 public static void init(){
 	for(int i=0;i<=15;i++){
 		for(int x = 0; x < group.length; x++){
-	        group[x] = new ArrayList<>();
+	        group[x] = new LinkedList<>();
 	    }
 	}
 }
@@ -105,8 +106,9 @@ public static void WyswietlListe(){
 		if(group[i].isEmpty()){
 			System.out.println("PRIORYTET: "+i);}
 		else{
-		for (int x=0;x<group[i].size();++x){	
 			System.out.println("PRIORYTET: "+i);
+		for (int x=0;x<group[i].size();++x){	
+			
 			//System.out.println(group[i]);
 			System.out.println("Proces: "+group[i].get(x).getID()+"  Priorytet: "+group[i].get(x).getPriorytet()+" Pozostaly czas:  " +group[i].get(x).getCzasPrzyjscia());
 		}
@@ -117,7 +119,7 @@ public static void WyswietlListe(){
 
 	public static void main(String[] args) {
 		int time = 0;
-		
+		int balans =0;
 		int id = 0;
 		Scanner scan = new Scanner(System.in);
 		int prio, size, klaw;
@@ -132,7 +134,7 @@ public static void WyswietlListe(){
 				prio = scan.nextInt();
 				System.out.print("Podaj dlugosc procesu: ");
 				size = scan.nextInt();
-				NowyProces(id,prio,size, time);
+				NowyProces(prio, size, id, time, size);
 				time--;
 				id++;
 				break;
@@ -149,7 +151,7 @@ public static void WyswietlListe(){
 					break;
 				}
 				
-				group[Priorytet].get(0).run();
+				group[Priorytet].getFirst().run();
 				
 				if(group[Priorytet].get(0).koniec ==true){
 					group[Priorytet].remove(0);
@@ -158,9 +160,34 @@ public static void WyswietlListe(){
 				else if (group[Priorytet].get(0).priorytet != group[Priorytet].get(0).priorytet_bazowy){
 					int tmp = group[Priorytet].get(0).getPriorytet();
 					int tmp2 = group[Priorytet].get(0).priorytet = group[Priorytet].get(0).priorytet-1;
+					group[group[Priorytet].get(0).getPriorytet()].push(group[Priorytet].get(0));
+					group[tmp].pollFirst();
+					SetZeroBitTabPriority(tmp);
+					NiePustaKolejka[tmp2]=true;
+				}
+				else{
+					group[Priorytet].addLast(group[Priorytet].getFirst());
+					group[Priorytet].removeFirst();
 				}
 				
+				balans++;
 				
+				if (balans== 5){
+					for (int i = 14; i>0;i--){
+						if (NiePustaKolejka[i] == true){
+							for (int j = 0; j!= group[i].size();){
+								if((time - group[i].get(j).czas_przyjscia)>= 9){
+									group[i].get(j).priorytet=15;
+									group[i].get(j).kwant=2;
+									group[15].addFirst(group[i].get(j));
+									SetZeroBitTabPriority(i);
+									NiePustaKolejka[15] = true;
+								}else j++;
+							}
+						}
+					}balans =0;
+				}
+				break;
 			}
 			}
 		}
